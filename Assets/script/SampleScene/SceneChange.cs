642179ceb108e55e884ff3f9cf77b72d03aafe35
xoghk9zla <xorbs9rla@naver.com>
2024-08-05 18:59:08 +0900

    dd

--- Assets/script/SampleScene/SceneChange.cs
+++ Assets/script/SampleScene/SceneChange.cs
@@ -9,6 +9,11 @@ public class SceneChange : MonoBehaviour
     [Header("게임이 끝났을 때 부분 버튼")]
     [SerializeField] Button AgainButton;//다시하기 버튼
     [SerializeField] Button LobiButton;//로비로 돌아가는 버튼
+    [Header("메뉴 버튼")]
+    [SerializeField] Button menuButton;
+    [SerializeField] Canvas MenuScene;
+    [SerializeField] Button LobbyScene;
+    [SerializeField] Button PlayButton;
 
     private void Awake()
     {
@@ -23,5 +28,23 @@ public class SceneChange : MonoBehaviour
         {
             SceneManager.LoadSceneAsync(0);
         });
+
+        menuButton.onClick.AddListener(() =>
+        {
+            Time.timeScale = 0;
+            MenuScene.gameObject.SetActive(true);
+        });
+
+        LobbyScene.onClick.AddListener(() =>
+        {
+            SceneManager.LoadSceneAsync(0);
+        });
+
+        PlayButton.onClick.AddListener(() =>
+        {
+            MenuScene.gameObject.SetActive(false);
+            Time.timeScale = 1;
+        });
+
     }
 }
